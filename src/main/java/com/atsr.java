777ba37d4f4c15732bfd517a82383838956d2287
sr <1820348786@qq.com>
2023-04-05 13:19:22 +0800

    hot-fix first commit

--- src/main/java/com/atsr.java
+++ src/main/java/com/atsr.java
@@ -3,5 +3,6 @@ package com;
 public class atsr {
     public static void main(String[] args) {
         System.out.println("hello3333");
+        System.out.println("hello hot-fix");
     }
 }
--- src/main/java/com/hello.java
+++ src/main/java/com/hello.java
@@ -3,5 +3,6 @@ package com;
 public class hello {
     public static void main(String[] args) {
         System.out.println("你好!3");
+        System.out.println("你好 hot-fix");
     }
 }
