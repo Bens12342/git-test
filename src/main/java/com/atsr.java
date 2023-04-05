8e937a859c97ea20ab2550a0776223355e80357f
sr <1820348786@qq.com>
2023-04-05 13:22:50 +0800

    master four commit

--- src/main/java/com/atsr.java
+++ src/main/java/com/atsr.java
@@ -2,7 +2,7 @@ package com;
 
 public class atsr {
     public static void main(String[] args) {
-        System.out.println("hello hot-fix 333");
+        System.out.println("hello master 333");
         System.out.println("hello hot-fix");
     }
 }
